1f521ce7f1e9162928d9be2b4f06af5576365d98
JunhoMyung <74249745+JunhoMyung@users.noreply.github.com>
2024-01-15 12:28:21 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
